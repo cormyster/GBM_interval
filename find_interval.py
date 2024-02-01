24e4674f589204f243e7daf9aa1410f2d577b74e
cormyster <cormyster12@gmail.com>
2024-02-01 07:39:18 +0000

    visualisation
    includes jupyter notebook containing plots to visualise the distribution and quantiles. Also prevent forex data from including weekends

--- find_interval.py
+++ find_interval.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 
-def simulate_stock_price(S0, mu, sigma, T=1, N=500):
-    dt = T/N  # time step
+def simulate_stock_price(S0, mu, sigma, T=1440, N=1440):
+    dt = T/N  # time step. default 1 minute
     dW = np.sqrt(dt) * np.random.randn(N)  # increments of Wiener process
     W = np.cumsum(dW)  # standard Brownian motion
 
@@ -13,28 +13,28 @@ def simulate_stock_price(S0, mu, sigma, T=1, N=500):
     
     return t, stock_price
 
-def interval(file):
+def interval(file,timeframe="D"):
     data = pd.read_csv(file,names=['time', 'complete', 'o', 'h', 'l', 'Close', 'v'])
+    times = {"M1":1,"M5":5,"M15":15,"M30":30,"H1":60,"H4":240,"D":1440,"W":10080,"M":43200}
     #data["Close"] = (data["Bid"] + data["Ask"])/2
     log_returns = np.log(data["Close"]/data["Close"].shift(1))
-    data["log returns"] = log_returns[1:]
+    data["log returns"] = log_returns
+    data = data[1:]
 
     sigma = np.sqrt(np.var(data["log returns"]))
-    mu = np.mean(data["log returns"]) + sigma**2/2
+    mu = np.mean(data["log returns"]) - sigma**2/2
     S0 = data["Close"].iloc[-1]
 
     final_prices = []
-    for i in range(10000):
-        t, stock_price = simulate_stock_price(S0, mu, sigma,T=len(data))
+    for i in range(100000):
+        t, stock_price = simulate_stock_price(S0, mu, sigma,T=times[timeframe], N=times[timeframe])
         final_prices.append(stock_price[-1])
 
     expected_value = np.mean(final_prices)
     #generate a confidence interval
     confidence_interval = np.percentile(final_prices, [0.1,1,99, 99.9])
     return confidence_interval,expected_value
-    print(f"Expected price: {expected_value}")
-    print(f"1% confidence interval: [{confidence_interval[1]}, {confidence_interval[2]}]")
-    print(f"0.1% confidence interval: [{confidence_interval[0]}, {confidence_interval[3]}]")
+
 
 if __name__ == "__main__":
     import os
--- t_bot.py
+++ t_bot.py
@@ -1,6 +1,7 @@
 import telebot
-from download_yesterday import download
+from download import download
 from find_interval import interval
+import os
 
 with open("BOT_KEY.txt", "r") as f:
     BOT_KEY = f.read()
@@ -15,22 +16,53 @@ def pong(message):
 
 @bot.message_handler(commands=['interval'])
 def get_interval(message):
-    instrument = message.text.split("/interval ")[1].upper()
-    #delete all files in data
-    import os
-    files = os.listdir("data")
-    for file in files:
-        print("delete" + file)
-        os.remove("data/"+file)
+    params = message.text.split("/interval ")[1].split(" -")
+    ticker = params[0].upper()
+    period = 1
+    timeframe = "D"
+    for p in params:
+        if p.startswith("tf "):
+            timeframe = p.split("tf ")[1]
+        elif p.startswith("p "):
+            period = int(p.split("p ")[1])
+
     try:
-        download(instrument)
-        while not os.path.exists(f"data/{instrument}.M1.out"):
+        download(ticker,period)
+        confidence_interval,expected_value = interval(f"data/{ticker}.M1.out",timeframe)
+        bot.send_message(message.chat.id,f"Expected price: {round(expected_value,5)}\n1% confidence interval: [{round(confidence_interval[1],5)}, {round(confidence_interval[2],5)}]\n0.1% confidence interval: [{round(confidence_interval[0],5)}, {round(confidence_interval[3],5)}]")
+        while not os.path.exists(f"data/{ticker}.M1.out"):
             print("not found")
-        confidence_interval,expected_value = interval(f"data/{instrument}.M1.out")
+        
     except Exception as e:
         bot.send_message(message.chat.id, f"Error: {e}")
 
-    bot.send_message(message.chat.id,f"Expected price: {round(expected_value,5)}\n1% confidence interval: [{round(confidence_interval[1],5)}, {round(confidence_interval[2],5)}]\n0.1% confidence interval: [{round(confidence_interval[0],5)}, {round(confidence_interval[3],5)}]")
+    
+@bot.message_handler(commands=['trade'])
+def trade(message):
+    ticker = message.text.split("/trade ")[1].upper()
+    try:
+        download(ticker)
+        confidence_interval,expected_value = interval(f"data/{ticker}.out")
+        bot.send_message(message.chat.id,f"Expected price: {round(expected_value,5)}\n1% confidence interval: [{round(confidence_interval[1],5)}, {round(confidence_interval[2],5)}]\n0.1% confidence interval: [{round(confidence_interval[0],5)}, {round(confidence_interval[3],5)}]")
+        while not os.path.exists(f"data/{ticker}.M1.out"):
+            print("not found")
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Error: {e}")
+
+@bot.message_handler(commands=['alert'])
+def alert(message):
+    ticker = message.text.split("/alert ")[1].upper()
+    try:
+        download(ticker)
+        confidence_interval,expected_value = interval(f"data/{ticker}.out")
+        print(confidence_interval)
+        path = r"C:\Users\cormy\AppData\Roaming\MetaQuotes\Terminal\3212703ED955F10C7534BE8497B221F4\MQL4\Files"
+        with open(f"{path}\{ticker}.txt", "w") as f:
+            f.write(f"{expected_value,confidence_interval[1]},{confidence_interval[2]},{confidence_interval[0]},{confidence_interval[3]}")
+            print("Written")
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Error: {e}")
 
 
+print("Start")
 bot.infinity_polling()
