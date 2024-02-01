b67190a99a5628b8213d741ab077f2791103c7b8
cormyster <cormyster12@gmail.com>
2024-02-01 07:40:50 +0000

    deleted redundant code

--- t_bot.py
+++ t_bot.py
@@ -37,31 +37,6 @@ def get_interval(message):
         bot.send_message(message.chat.id, f"Error: {e}")
 
     
-@bot.message_handler(commands=['trade'])
-def trade(message):
-    ticker = message.text.split("/trade ")[1].upper()
-    try:
-        download(ticker)
-        confidence_interval,expected_value = interval(f"data/{ticker}.out")
-        bot.send_message(message.chat.id,f"Expected price: {round(expected_value,5)}\n1% confidence interval: [{round(confidence_interval[1],5)}, {round(confidence_interval[2],5)}]\n0.1% confidence interval: [{round(confidence_interval[0],5)}, {round(confidence_interval[3],5)}]")
-        while not os.path.exists(f"data/{ticker}.M1.out"):
-            print("not found")
-    except Exception as e:
-        bot.send_message(message.chat.id, f"Error: {e}")
-
-@bot.message_handler(commands=['alert'])
-def alert(message):
-    ticker = message.text.split("/alert ")[1].upper()
-    try:
-        download(ticker)
-        confidence_interval,expected_value = interval(f"data/{ticker}.out")
-        print(confidence_interval)
-        path = r"C:\Users\cormy\AppData\Roaming\MetaQuotes\Terminal\3212703ED955F10C7534BE8497B221F4\MQL4\Files"
-        with open(f"{path}\{ticker}.txt", "w") as f:
-            f.write(f"{expected_value,confidence_interval[1]},{confidence_interval[2]},{confidence_interval[0]},{confidence_interval[3]}")
-            print("Written")
-    except Exception as e:
-        bot.send_message(message.chat.id, f"Error: {e}")
 
 
 print("Start")
